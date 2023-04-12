ba07232ba30f32fe45934a50a84d4423143fe18e
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-04-12 05:12:01 +0100

    Initial commit

---
+++ src/main/java/com/antares/jinsei/JinseiApplication.java
@@ -0,0 +1,13 @@
+package com.antares.jinsei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinseiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JinseiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antares/jinsei/JinseiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antares.jinsei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JinseiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
