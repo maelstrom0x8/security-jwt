09865728f242c9be24c8e1c06934a20dc19726b8
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-04-20 12:08:13 +0100

    Add test for controller

--- src/test/java/com/antares/jinsei/JinseiApplicationTests.java
+++ src/test/java/com/antares/jinsei/JinseiApplicationTests.java
@@ -1,13 +1,29 @@
 package com.antares.jinsei;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.antares.jinsei.controller.HelloController;
+
+@WebMvcTest(controllers = HelloController.class)
+@WithMockUser(username = "alice", password = "12345")
 class JinseiApplicationTests {
 
+	@Autowired
+	private MockMvc mvc;
+
 	@Test
-	void contextLoads() {
+	void testHello() throws Exception {
+		mvc.perform(get("/hello"))
+				.andExpect(content().string("Hello!"))
+				.andExpect(status().isOk());
 	}
 
 }
