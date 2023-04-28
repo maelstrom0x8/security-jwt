6aab9716343e9eb1841cb0747db2d2f62d2eff13
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-04-28 10:38:28 +0100

    Remove authentication filters

--- src/main/java/com/antares/jinsei/security/WebSecurityConfiguration.java
+++ src/main/java/com/antares/jinsei/security/WebSecurityConfiguration.java
@@ -2,13 +2,9 @@ package com.antares.jinsei.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-
-import com.antares.jinsei.security.filter.AuthenticationLoggingFilter;
-import com.antares.jinsei.security.filter.RequestValidationFilter;
-import com.antares.jinsei.security.filter.StaticKeyAuthenticationFilter;
 
 @Configuration
 public class WebSecurityConfiguration {
@@ -16,10 +12,7 @@ public class WebSecurityConfiguration {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.addFilterAt(new StaticKeyAuthenticationFilter(), BasicAuthenticationFilter.class);
-
-        http.addFilterBefore(new RequestValidationFilter(), BasicAuthenticationFilter.class);
-        http.addFilterAfter(new AuthenticationLoggingFilter(), BasicAuthenticationFilter.class);
+        http.httpBasic(Customizer.withDefaults());
 
         http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
