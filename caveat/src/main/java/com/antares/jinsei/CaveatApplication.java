a75a175fe90f4a5fbe8b36d648ee9719f30b76a5
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-04-29 21:50:21 +0100

    Create subprojects

--- caveat/src/main/java/com/antares/jinsei/CaveatApplication.java
+++ caveat/src/main/java/com/antares/jinsei/CaveatApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JinseiApplication {
+public class CaveatApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JinseiApplication.class, args);
+		SpringApplication.run(CaveatApplication.class, args);
 	}
 
 }
